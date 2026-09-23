716eeb7fe28468186fa2c888fe94bea45e4639da
masterhassanmuhammad-glitch <masterhassanmuhammad@gmail.com>
2026-09-23 11:13:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 # إعداد نموذج Gemini
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-3.7-flash")
 
 @app.route('/', methods=['GET'])
 def home():
